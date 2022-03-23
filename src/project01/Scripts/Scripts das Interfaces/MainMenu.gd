6750c559612331f715feb2300f22d6c093e811be
luizpcferreira <99211976+luizpcferreira@users.noreply.github.com>
2022-03-23 09:41:16 -0300

    botao e intro
    botao e intro

--- src/project01/Scripts/Scripts das Interfaces/MainMenu.gd
+++ src/project01/Scripts/Scripts das Interfaces/MainMenu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+onready var Intro = $VideoPlayer
+
+onready var musica = $AudioStreamPlayer
+
 func _ready():
 	pass
 
@@ -9,3 +13,7 @@ func _on_Button3_pressed():
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://Cenas/Interface do Usuário/EscolhaPoder.tscn") #Começa o Jogo
+
+func _on_VideoPlayer_finished():
+	Intro.queue_free()
+	musica.playing = true
